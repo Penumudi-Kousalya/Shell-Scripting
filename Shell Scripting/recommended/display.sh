05d95066d7ca01c3afaec06f344e3765724738dc
Penumudi-Kousalya <140244572+Penumudi-Kousalya@users.noreply.github.com>
2024-11-20 22:51:42 +0530

    Add files via upload

---
+++ Shell Scripting/recommended/display.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+samplefile="samplefile"
+
+if [ ! -f "$samplefile" ]; then
+    echo "Error: File '$samplefile' does not exist."
+    exit 1
+fi
+
+# Display all lines except the last one
+head -n -1 "$samplefile"
